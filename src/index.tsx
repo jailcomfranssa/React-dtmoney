905be24fea5f05760a15e50a20b2d66dd336b10b
jailcomfranssa <jail_son_0@hotmail.com>
2023-08-03 16:39:56 -0300

    add e Configurando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs"
 import { App } from "./App";
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () =>{
+      return[
+        {
+          id: 1,
+          title: 'Transação 01',
+          amount: 400,
+          category: 'Coida',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
